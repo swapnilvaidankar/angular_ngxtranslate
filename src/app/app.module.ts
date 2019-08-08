5e28d8b9f796e4db515e47389040324679f1e42e
SwapnilVaidankar <swapnilvaidankar@gmail.com>
2019-08-08 21:19:53 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+/* Multi lang */
+import { TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import { TranslateHttpLoader} from "@ngx-translate/http-loader";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+
+/**Multi Lang */
+import { AppComponent } from './app.component';
+
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
